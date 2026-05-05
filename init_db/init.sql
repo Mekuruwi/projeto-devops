a830aff90d1262e2d72c2a3e4a5ad06bd7649391
Mekuruwi <kevinwilliamdossantosrazera@gmail.com>
2026-05-05 13:48:01 -0300

    Na tese finalizado

--- init_db/init.sql
+++ init_db/init.sql
@@ -1,4 +1,4 @@
-CREATE IF NOT EXISTS projeto_devops;
+CREATE DATABASE IF NOT EXISTS projeto_devops;
 USE projeto_devops;
 
 create table if not exists produtos(
@@ -17,3 +17,13 @@ quantidade int not null,
 estoque int default 0,
 Data_registro datetime default current_timestamp,
 mes_ano varchar(10) not null) engine=innodb;
+
+create table if not exists users(
+    id int auto_increment primary key,
+    username varchar(50) unique not null,
+    email varchar(255) not null,
+    password_hash varchar(255) not null,
+    created_at datetime default current_timestamp
+)
+
+insert into users (username, email, password_hash) values ('admin', 'admin@admin.com', password_hash('admin123', password_default()));
--- init_db/users.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists users(
-    id int auto_increment primary key,
-    username varchar(50) unique not null,
-    email varchar(255) not null,
-    password_hash varchar(255) not null,
-    created_at datetime default current_timestamp
-)
-
-insert into users (username, email, password_hash) values ('admin', 'admin@admin.com', password_hash('admin123', password_default()));
